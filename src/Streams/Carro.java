041113286150bc2c886edb9d9dd0d038a4005cd7
Gabriel vettorazzi <gabrielvettorazzii@gmail.com>
2025-04-01 22:30:33 -0300

    #stream API - ex Filter

---
+++ src/Streams/Carro.java
@@ -0,0 +1,12 @@
+package Streams;
+
+public class Carro {
+     final String nome;
+     final double velocidade;
+
+    public Carro(String nome, double velocidade) {
+        this.nome = nome;
+        this.velocidade = velocidade;
+    }
+
+}
---
+++ src/Streams/FilterDesafio.java
@@ -0,0 +1,22 @@
+package Streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FilterDesafio {
+    public static void main(String[] args) {
+        //crie duas Listas para urtilizar oo filter
+
+        Carro C1 = new Carro("CAMARO", 233);
+        Carro C2 = new Carro("ferrari", 280);
+        Carro C3 = new Carro("AUDI", 190);
+        Carro C4 = new Carro("FIAT UNO", 460);
+
+        List<Carro> caros = Arrays.asList(C1,C2,C3,C4);
+          caros.stream()
+                  .filter(c -> c.velocidade >= 280)
+                  .map(c -> "O carro mais rapido do mundo é: " + c +"!!")
+                  .forEach(System.out::println);
+
+    }
+}
